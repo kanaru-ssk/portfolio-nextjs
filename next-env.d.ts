cfaacc24f24de940bce2b6cc35bdd76566c648b8
kanaru-ssk <2019january.0131@gmail.com>
2022-05-14 01:11:58 +0900

    環境変数の型定義追加

--- next-env.d.ts
+++ next-env.d.ts
@@ -3,3 +3,11 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
+
+// 環境変数の型定義
+namespace NodeJS {
+	interface ProcessEnv {
+		readonly NEXT_PUBLIC_MICROCMS_API_KEY: string;
+		readonly NEXT_PUBLIC_MICROCMS_ENDPOINT_CUSTOM: string;
+	}
+}
