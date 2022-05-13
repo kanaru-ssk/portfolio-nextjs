f16968879a8d10fe1f9239ceaf94fabfa4ffd40b
kanaru-ssk <2019january.0131@gmail.com>
2022-05-14 00:22:05 +0900

    edit test

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,6 +15,7 @@ const Home: NextPage = () => {
 			<main className={styles.main}>
 				<h1 className={styles.title}>
 					Welcome to <a href="https://nextjs.org">Next.js!</a>
+					<p>編集テスト</p>
 				</h1>
 
 				<p className={styles.description}>
