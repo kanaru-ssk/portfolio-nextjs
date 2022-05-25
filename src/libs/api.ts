2e55680b8ad7935f415bce65d740350941edefcb
kanaru-ssk <2019january.0131@gmail.com>
2022-05-26 01:38:17 +0900

    localhostのapiを叩かないよう修正

--- src/libs/api.ts
+++ src/libs/api.ts
@@ -1,15 +1,6 @@
 import qs from 'qs';
 
 /**
- * Get full Strapi URL from path
- * @param {string} path Path of the URL
- * @returns {string} Full Strapi URL
- */
-export const getStrapiURL = (path = '') => {
-	return `${process.env.NEXT_PUBLIC_STRAPI_API_URL || 'http://localhost:1337'}${path}`;
-};
-
-/**
  * Helper to make GET requests to Strapi API endpoints
  * @param {string} path Path of the API route
  * @param {Object} urlParamsObject URL params object, will be stringified
@@ -27,7 +18,7 @@ export const fetchAPI = async (path: string, urlParamsObject = {}, options = {})
 
 	// Build request URL
 	const queryString = qs.stringify(urlParamsObject);
-	const requestUrl = `${getStrapiURL(`/api/${path}${queryString ? `?${queryString}` : ''}`)}`;
+	const requestUrl = process.env.NEXT_PUBLIC_STRAPI_API_URL + `/api/${path}${queryString ? `?${queryString}` : ''}`;
 
 	// Trigger API call
 	const response = await fetch(requestUrl, mergedOptions);
