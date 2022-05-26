d42a10eb51a07aee49443cec7c9479117a8064b4
kanaru-ssk <2019january.0131@gmail.com>
2022-05-26 23:33:35 +0900

    sitemap に link 追加

--- src/components/common/Footer/Logo.tsx
+++ src/components/common/Footer/Logo.tsx
@@ -1,7 +1,13 @@
+import Link from 'next/link';
+
 const Logo = () => {
 	return (
 		<div className="text-center pt-16 pb-8">
-			<amp-img width="96" height="48" src="/logo-white.svg" alt="logo" />
+			<Link href="/">
+				<a>
+					<amp-img width="96" height="48" src="/logo-white.svg" alt="logo" />
+				</a>
+			</Link>
 		</div>
 	);
 };
--- src/components/common/Footer/SiteMap.tsx
+++ src/components/common/Footer/SiteMap.tsx
@@ -1,10 +1,28 @@
+import Link from 'next/link';
+
 const SiteMap = () => {
 	return (
-		<div className="flex justify-between px-4 py-8">
-			<div>about</div>
-			<div>products</div>
-			<div>works</div>
-			<div>contact</div>
+		<div className="flex justify-between px-4 py-8 font-medium">
+			<div>
+				<Link href="/about">
+					<a>about</a>
+				</Link>
+			</div>
+			<div>
+				<Link href="/products">
+					<a>products</a>
+				</Link>
+			</div>
+			<div>
+				<Link href="/works">
+					<a>works</a>
+				</Link>
+			</div>
+			<div>
+				<Link href="/contact">
+					<a>contact</a>
+				</Link>
+			</div>
 		</div>
 	);
 };
