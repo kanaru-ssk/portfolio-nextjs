fc5a24249c2f710e78a63ee9d1bf4ea226aecdc6
kanaru-ssk <2019january.0131@gmail.com>
2022-05-27 01:22:39 +0900

    Footer logo padding 調整

--- src/components/common/Footer/Logo.tsx
+++ src/components/common/Footer/Logo.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 const Logo = () => {
 	return (
-		<div className="text-center pt-16 pb-8">
+		<div className="text-center py-16">
 			<Link href="/">
 				<a>
 					<amp-img width="96" height="48" src="/logo-white.svg" alt="logo" />
