1c5aef4f440a15a14ad0c33385a3f8d89e9f5ac5
kanaru-ssk <2019january.0131@gmail.com>
2022-05-26 22:50:10 +0900

    getInitialProps 削除

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,27 +8,15 @@ import Header from 'components/common/Header';
 import Footer from 'components/common/Footer';
 
 class MyDocument extends Document {
-	static async getInitialProps(ctx: any) {
-		const initialProps: any = await Document.getInitialProps(ctx);
-		return {
-			...initialProps,
-			styles: (
-				<>
-					{initialProps.styles}
+	render() {
+		return (
+			<Html lang="ja">
+				<Head>
 					<style
 						dangerouslySetInnerHTML={{
 							__html: outputcss,
 						}}
 					/>
-				</>
-			),
-		};
-	}
-
-	render() {
-		return (
-			<Html lang="ja">
-				<Head>
 					<link rel="icon" href="/favicon.svg" />
 				</Head>
 				<body>
