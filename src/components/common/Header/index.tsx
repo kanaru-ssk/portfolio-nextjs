903cbb3a0c32bf980e1f014efa2392ebf987b480
kanaru-ssk <2019january.0131@gmail.com>
2022-05-27 12:47:53 +0900

    headerのz-index調整

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 const Header = () => {
 	return (
-		<header className="fixed flex justify-between items-center w-full h-20 px-4 bg-white">
+		<header className="z-10 fixed flex justify-between items-center w-full h-20 px-4 bg-white">
 			<Link href="/">
 				<a>
 					<amp-img width="64" height="32" src="/logo.svg" alt="logo" />
