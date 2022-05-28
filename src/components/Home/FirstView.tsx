6a7cb765ccfef50a25326ba70a7aed18fac41d9f
kanaru-ssk <2019january.0131@gmail.com>
2022-05-29 00:47:37 +0900

    <br  /> にkey追加

--- src/components/Home/FirstView.tsx
+++ src/components/Home/FirstView.tsx
@@ -5,10 +5,10 @@ type Props = {
 const FirstView = ({ catchCopy }: Props) => {
 	// 置換 : \n -> <br />>
 	const lbToBr = (txt: string) => {
-		return txt.split(/(\n)/g).map((t) => (t === '\n' ? <br /> : t));
+		return txt.split(/(\n)/g).map((value, key) => (value === '\n' ? <br key={key} /> : value));
 	};
 
-	return <div className="flex flex-col  justify-center h-screen ">{lbToBr(catchCopy)}</div>;
+	return <div className="flex flex-col justify-center h-screen">{lbToBr(catchCopy)}</div>;
 };
 
 export default FirstView;
