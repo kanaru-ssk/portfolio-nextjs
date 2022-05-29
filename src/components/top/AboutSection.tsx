ada355ab98f7502607fef174beeeb3a1e2dc4574
kanaru-ssk <2019january.0131@gmail.com>
2022-05-29 22:24:59 +0900

    Profile componentを表示するよう修正

--- src/components/top/AboutSection.tsx
+++ src/components/top/AboutSection.tsx
@@ -1,33 +1,20 @@
-import { AboutPage } from 'types/aboutPage';
+import MoreButton from 'components/common/MoreButton';
+import Profile from 'components/about/Profile';
 
 type Props = {
-	contents: AboutPage;
+	profileImg: string;
+	name: string;
+	nameKana: string;
+	job: string;
+	profileText: string;
 };
 
-const AboutSection = ({ contents }: Props) => {
-	// \n -> <br />>
-	const lbToBr = (txt: string) => {
-		return txt.split(/(\n)/g).map((value, key) => (value === '\n' ? <br key={key} /> : value));
-	};
-
+const AboutSection = ({ profileImg, name, nameKana, job, profileText }: Props) => {
 	return (
-		<div>
-			<h1 className="py-4">about</h1>
-			<div className="flex items-center gap-8 py-4">
-				<amp-img
-					className="rounded-full"
-					src={contents.profile_img.data.attributes.url}
-					width="128"
-					height="128"
-					alt="profile"
-				/>
-				<div>
-					<div>{contents.job}</div>
-					<div className="text-xl">{contents.name}</div>
-					<div>{contents.name_kana}</div>
-				</div>
-			</div>
-			<p className="py-4">{lbToBr(contents.profile_text)}</p>
+		<div className="py-16">
+			<h2 className="py-4">about</h2>
+			<Profile profileImg={profileImg} name={name} nameKana={nameKana} job={job} profileText={profileText} />
+			<MoreButton url="/about" />
 		</div>
 	);
 };
