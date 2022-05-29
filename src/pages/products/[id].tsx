87a0e7fce5dbc75924cdba968a5ef932567665f8
kanaru-ssk <2019january.0131@gmail.com>
2022-05-29 19:44:34 +0900

    個別ページのパスをCMSで管理できるよう変更

--- src/pages/products/[id].tsx
+++ src/pages/products/[id].tsx
@@ -1,38 +1,81 @@
 import type { NextPage, GetStaticProps } from 'next';
+import Head from 'next/head';
+const EditorJSHtml = require('editorjs-html');
 
+// lib
 import { fetchAPI } from 'libs/api';
+import { CommonRes, Common } from 'types/common';
+import { AboutPageRes, AboutPage } from 'types/aboutPage';
+import { ProductsRes, Products, Product } from 'types/products';
+
+// components
+import Header from 'components/common/Header';
+import Footer from 'components/common/Footer';
 
 export const config = { amp: true };
 
 type Props = {
-	articles: any;
+	common: Common;
+	about: AboutPage;
+	product: Product;
 };
 
-const ProductsArticle: NextPage<Props> = ({ articles }: Props) => {
-	console.log(articles);
+const WorksArticle: NextPage<Props> = ({ common, about, product }: Props) => {
+	const contents: string[] = EditorJSHtml().parse(JSON.parse(product.contents));
 	return (
 		<>
-			<div className="h-20"></div>
-			<div className="p-4">{articles.contents}</div>
+			<Head>
+				<link rel="canonical" href={process.env.NEXT_PUBLIC_PRODUCTION_DOMAIN + '/works'} />
+				<link rel="icon" href={common.favicon.data.attributes.url} />
+
+				{/* ogp */}
+				<meta property="og:url" content={process.env.NEXT_PUBLIC_DOMAIN} />
+				<meta property="og:type" content="website" />
+				<meta property="og:image" content={product.ogp_img.data.attributes.url} />
+				<meta property="og:title" content={product.title} />
+				<meta property="og:description" content={product.description} />
+				<meta name="twitter:card" content="summary" />
+
+				<title>{product.title}</title>
+				<meta name="description" content={product.description} />
+			</Head>
+
+			<Header logo={common.header_logo.data.attributes.url} />
+			<main className="p-4">
+				<div className="h-20"></div>
+
+				{contents.map((value, key) => {
+					return <div key={key} dangerouslySetInnerHTML={{ __html: value }}></div>;
+				})}
+			</main>
+			<Footer logo={common.logo_white.data.attributes.url} copyRight={common.copy_right} snsLinks={about.links} />
 		</>
 	);
 };
 
-export default ProductsArticle;
+export default WorksArticle;
 
 export const getStaticPaths = async () => {
-	const res: any = await fetchAPI('products');
-	const paths = res.data.map((article: any) => `/products/${article.id}`);
+	const productsRes: ProductsRes = await fetchAPI('products');
+	const paths = productsRes.data.map((products: Products) => `/products/${products.attributes.path}`);
 	return { paths, fallback: false };
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-	const resArticles: any = await fetchAPI(`products/${params!.id}`);
+	const commonRes: CommonRes = await fetchAPI('common', { populate: '*' });
+	const aboutRes: AboutPageRes = await fetchAPI('about-page', {
+		populate: { links: { populate: '*' }, profile_img: { populate: '*' }, ogp_img: { populate: '*' } },
+	});
+	const productsRes: ProductsRes = await fetchAPI('products', { filter: { path: params!.id }, populate: '*' });
 
-	const articles = resArticles.data.attributes;
+	const common: Common = commonRes.data.attributes;
+	const about: AboutPage = aboutRes.data.attributes;
+	const product: Product = productsRes.data[0].attributes;
 	return {
 		props: {
-			articles,
+			common,
+			about,
+			product,
 		},
 	};
 };
--- src/pages/works/[id].tsx
+++ src/pages/works/[id].tsx
@@ -1,19 +1,54 @@
 import type { NextPage, GetStaticProps } from 'next';
+import Head from 'next/head';
+const EditorJSHtml = require('editorjs-html');
 
+// lib
 import { fetchAPI } from 'libs/api';
+import { CommonRes, Common } from 'types/common';
+import { AboutPageRes, AboutPage } from 'types/aboutPage';
+import { WorksRes, Works, Work } from 'types/works';
+
+// components
+import Header from 'components/common/Header';
+import Footer from 'components/common/Footer';
 
 export const config = { amp: true };
 
 type Props = {
-	articles: any;
+	common: Common;
+	about: AboutPage;
+	work: Work;
 };
 
-const WorksArticle: NextPage<Props> = ({ articles }: Props) => {
-	console.log('aa');
+const WorksArticle: NextPage<Props> = ({ common, about, work }: Props) => {
+	const contents: string[] = EditorJSHtml().parse(JSON.parse(work.contents));
 	return (
 		<>
-			<div className="h-20"></div>
-			<div className="p-4">{articles.contents}</div>
+			<Head>
+				<link rel="canonical" href={process.env.NEXT_PUBLIC_PRODUCTION_DOMAIN + '/works'} />
+				<link rel="icon" href={common.favicon.data.attributes.url} />
+
+				{/* ogp */}
+				<meta property="og:url" content={process.env.NEXT_PUBLIC_DOMAIN} />
+				<meta property="og:type" content="website" />
+				<meta property="og:image" content={work.ogp_img.data.attributes.url} />
+				<meta property="og:title" content={work.title} />
+				<meta property="og:description" content={work.description} />
+				<meta name="twitter:card" content="summary" />
+
+				<title>{work.title}</title>
+				<meta name="description" content={work.description} />
+			</Head>
+
+			<Header logo={common.header_logo.data.attributes.url} />
+			<main className="p-4">
+				<div className="h-20"></div>
+
+				{contents.map((value, key) => {
+					return <div key={key} dangerouslySetInnerHTML={{ __html: value }}></div>;
+				})}
+			</main>
+			<Footer logo={common.logo_white.data.attributes.url} copyRight={common.copy_right} snsLinks={about.links} />
 		</>
 	);
 };
@@ -21,18 +56,26 @@ const WorksArticle: NextPage<Props> = ({ articles }: Props) => {
 export default WorksArticle;
 
 export const getStaticPaths = async () => {
-	const res: any = await fetchAPI('works');
-	const paths = res.data.map((article: any) => `/works/${article.id}`);
+	const worksRes: WorksRes = await fetchAPI('works');
+	const paths = worksRes.data.map((works: Works) => `/works/${works.attributes.path}`);
 	return { paths, fallback: false };
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-	const resArticles: any = await fetchAPI(`works/${params!.id}`);
+	const commonRes: CommonRes = await fetchAPI('common', { populate: '*' });
+	const aboutRes: AboutPageRes = await fetchAPI('about-page', {
+		populate: { links: { populate: '*' }, profile_img: { populate: '*' }, ogp_img: { populate: '*' } },
+	});
+	const worksRes: WorksRes = await fetchAPI('works', { filter: { path: params!.id }, populate: '*' });
 
-	const articles = resArticles.data.attributes;
+	const common: Common = commonRes.data.attributes;
+	const about: AboutPage = aboutRes.data.attributes;
+	const work: Work = worksRes.data[0].attributes;
 	return {
 		props: {
-			articles,
+			common,
+			about,
+			work,
 		},
 	};
 };
