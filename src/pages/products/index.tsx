3be1b276f0b8e0b62738e0adc33de12ef2bd6d1a
kanaru-ssk <2019january.0131@gmail.com>
2022-05-29 15:00:58 +0900

    各ページのコンテンツをapiから取得するよう変更

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,7 +1,74 @@
+import type { NextPage, GetStaticProps } from 'next';
+import Head from 'next/head';
+const EditorJSHtml = require('editorjs-html');
+
+// lib
+import { fetchAPI } from 'libs/api';
+import { CommonRes, Common } from 'types/common';
+import { AboutPageRes, AboutPage } from 'types/aboutPage';
+import { ProductsPageRes, ProductsPage } from 'types/productsPage';
+
+// components
+import Header from 'components/common/Header';
+import Footer from 'components/common/Footer';
+
 export const config = { amp: true };
 
-const Products = () => {
-	return <div className="flex items-center h-screen p-4">products page</div>;
+type Props = {
+	common: Common;
+	about: AboutPage;
+	products: ProductsPage;
+};
+
+const Products: NextPage<Props> = ({ common, about, products }: Props) => {
+	const contents: string[] = EditorJSHtml().parse(JSON.parse(products.contents));
+	return (
+		<>
+			<Head>
+				<link rel="canonical" href={process.env.NEXT_PUBLIC_DOMAIN} />
+				<link rel="icon" href={common.favicon.data.attributes.url} />
+
+				{/* ogp */}
+				<meta property="og:url" content={process.env.NEXT_PUBLIC_DOMAIN} />
+				<meta property="og:type" content="website" />
+				<meta property="og:image" content={products.ogp_img.data.attributes.url} />
+				<meta property="og:title" content={products.title} />
+				<meta property="og:description" content={products.description} />
+				<meta name="twitter:card" content="summary" />
+
+				<title>{products.title}</title>
+				<meta name="description" content={products.description} />
+			</Head>
+			<Header logo={common.header_logo.data.attributes.url} />
+			<main className="p-4">
+				<div className="h-20"></div>
+
+				{contents.map((value, key) => {
+					return <div key={key} dangerouslySetInnerHTML={{ __html: value }}></div>;
+				})}
+			</main>
+			<Footer logo={common.logo_white.data.attributes.url} copyRight={common.copy_right} snsLinks={about.links} />
+		</>
+	);
 };
 
 export default Products;
+
+export const getStaticProps: GetStaticProps = async () => {
+	const commonRes: CommonRes = await fetchAPI('common', { populate: '*' });
+	const aboutRes: AboutPageRes = await fetchAPI('about-page', {
+		populate: { links: { populate: '*' }, profile_img: { populate: '*' }, ogp_img: { populate: '*' } },
+	});
+	const productsRes: ProductsPageRes = await fetchAPI('products-page', { populate: '*' });
+
+	const common: Common = commonRes.data.attributes;
+	const about: AboutPage = aboutRes.data.attributes;
+	const products: ProductsPage = productsRes.data.attributes;
+	return {
+		props: {
+			common,
+			about,
+			products,
+		},
+	};
+};
--- src/pages/works/index.tsx
+++ src/pages/works/index.tsx
@@ -1,29 +1,74 @@
-import type { NextPage } from 'next';
+import type { NextPage, GetStaticProps } from 'next';
 import Head from 'next/head';
+const EditorJSHtml = require('editorjs-html');
+
+// lib
+import { fetchAPI } from 'libs/api';
+import { CommonRes, Common } from 'types/common';
+import { AboutPageRes, AboutPage } from 'types/aboutPage';
+import { WorksPageRes, WorksPage } from 'types/worksPage';
+
+// components
+import Header from 'components/common/Header';
+import Footer from 'components/common/Footer';
 
 export const config = { amp: true };
 
-const Works: NextPage = () => {
+type Props = {
+	common: Common;
+	about: AboutPage;
+	works: WorksPage;
+};
+
+const Works: NextPage<Props> = ({ common, about, works }: Props) => {
+	const contents: string[] = EditorJSHtml().parse(JSON.parse(works.contents));
 	return (
 		<>
 			<Head>
+				<link rel="canonical" href={process.env.NEXT_PUBLIC_DOMAIN} />
+				<link rel="icon" href={common.favicon.data.attributes.url} />
+
 				{/* ogp */}
 				<meta property="og:url" content={process.env.NEXT_PUBLIC_DOMAIN} />
 				<meta property="og:type" content="website" />
-				<meta property="og:image" content={process.env.NEXT_PUBLIC_DOMAIN + '/ogp.png'} />
-				<meta property="og:title" content="webエンジニア 佐々木哉瑠(かなる)の公式ホームページ" />
-				<meta
-					property="og:description"
-					content="仙台で活動するweb開発エンジニア 佐々木かなるの公式ホームページです。"
-				/>
+				<meta property="og:image" content={works.ogp_img.data.attributes.url} />
+				<meta property="og:title" content={works.title} />
+				<meta property="og:description" content={works.description} />
 				<meta name="twitter:card" content="summary" />
 
-				<title>webエンジニア佐々木哉瑠(かなる)の公式ホームページ【作品一覧】</title>
-				<meta name="description" content="仙台のweb開発エンジニア 佐々木かなるのポートフォリオサイト" />
+				<title>{works.title}</title>
+				<meta name="description" content={works.description} />
 			</Head>
-			<div className="flex items-center h-screen p-4">works page</div>
+			<Header logo={common.header_logo.data.attributes.url} />
+			<main className="p-4">
+				<div className="h-20"></div>
+
+				{contents.map((value, key) => {
+					return <div key={key} dangerouslySetInnerHTML={{ __html: value }}></div>;
+				})}
+			</main>
+			<Footer logo={common.logo_white.data.attributes.url} copyRight={common.copy_right} snsLinks={about.links} />
 		</>
 	);
 };
 
 export default Works;
+
+export const getStaticProps: GetStaticProps = async () => {
+	const commonRes: CommonRes = await fetchAPI('common', { populate: '*' });
+	const aboutRes: AboutPageRes = await fetchAPI('about-page', {
+		populate: { links: { populate: '*' }, profile_img: { populate: '*' }, ogp_img: { populate: '*' } },
+	});
+	const worksRes: WorksPageRes = await fetchAPI('works-page', { populate: '*' });
+
+	const common: Common = commonRes.data.attributes;
+	const about: AboutPage = aboutRes.data.attributes;
+	const works: WorksPage = worksRes.data.attributes;
+	return {
+		props: {
+			common,
+			about,
+			works,
+		},
+	};
+};
