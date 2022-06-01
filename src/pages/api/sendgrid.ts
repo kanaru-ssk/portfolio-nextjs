a92f57e8a14adb7e69b8ce5d6fb4482746314c3b
kanaru-ssk <2019january.0131@gmail.com>
2022-06-01 14:22:32 +0900

    お問い合わせメール内容修正

--- src/pages/api/sendgrid.ts
+++ src/pages/api/sendgrid.ts
@@ -1,6 +1,5 @@
 import sgMail from '@sendgrid/mail';
 import { MailDataRequired } from '@sendgrid/helpers/classes/mail';
-import { EmailData } from '@sendgrid/helpers/classes/email-address';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 const handler = (req: NextApiRequest, res: NextApiResponse<any>) => {
@@ -13,15 +12,26 @@ const handler = (req: NextApiRequest, res: NextApiResponse<any>) => {
 			subject: '【自動返信】お問合せありがとうございました。',
 			text:
 				'お問合せを受け付けました。回答をお待ちください。\n\n' +
-				'セキュリティの関係上、お問い合わせ内容は自動返信メールに記載しておりません。',
+				'セキュリティの関係上、お問い合わせ内容は自動返信メールに記載しておりません。\n\n' +
+				'佐々木哉瑠\n' +
+				'mail : ' +
+				process.env.CONTACT_MAIL_ADDRESS,
 		};
 		const toContact: MailDataRequired = {
 			to: process.env.CONTACT_MAIL_ADDRESS,
 			from: process.env.FORM_MAIL_ADDRESS,
-			subject: '【自動返信】お問合せありがとうございました。',
+			subject: 'お問合せ',
 			text:
+				'お問合せを受け付けました。\n\n' +
+				'本文 : \n' +
+				req.body.message +
+				'\n\n' +
+				'自動返信メール : \n' +
 				'お問合せを受け付けました。回答をお待ちください。\n\n' +
-				'セキュリティの関係上、お問い合わせ内容は自動返信メールに記載しておりません。',
+				'セキュリティの関係上、お問い合わせ内容は自動返信メールに記載しておりません。\n\n' +
+				'佐々木哉瑠\n' +
+				'mail : ' +
+				process.env.CONTACT_MAIL_ADDRESS,
 		};
 
 		(async () => {
