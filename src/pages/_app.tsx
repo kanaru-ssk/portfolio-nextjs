a5a0d88b7ad39f12e7c7c57769fe5d4cf823857b
kanaru-ssk <2019january.0131@gmail.com>
2022-07-10 17:24:31 +0900

    tailwindのimport方法を変更

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,22 @@
 import Head from 'next/head';
 import type { AppProps } from 'next/app';
-import 'styles/output.css';
+import 'styles/globals.css';
 
 import Header from 'components/common/Header';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-	return (
-		<>
-			<Head>
-				<meta name="viewport" content="initial-scale=1.0, width=device-width, maximum-scale=1.0" />
-				<meta name="theme-color" content="#232C93" />
-			</Head>
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width, maximum-scale=1.0" />
+        <meta name="theme-color" content="#232C93" />
+      </Head>
 
-			<Header />
+      <Header />
 
-			<Component {...pageProps} />
-		</>
-	);
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default MyApp;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,38 +1,18 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 import React from 'react';
-// @ts-ignore
-import outputcss from '!raw-loader!../styles/output.css';
 
 class MyDocument extends Document {
-	static async getInitialProps(ctx: any) {
-		const initialProps: any = await Document.getInitialProps(ctx);
-
-		return {
-			...initialProps,
-			styles: (
-				<>
-					{initialProps.styles}
-					<style
-						dangerouslySetInnerHTML={{
-							__html: outputcss,
-						}}
-					/>
-				</>
-			),
-		};
-	}
-
-	render() {
-		return (
-			<Html lang="ja">
-				<Head />
-				<body>
-					<Main />
-					<NextScript />
-				</body>
-			</Html>
-		);
-	}
+  render() {
+    return (
+      <Html lang="ja">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
 
 export default MyDocument;
