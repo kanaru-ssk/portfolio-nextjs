c40279a12dae83239d8d69021692dc8d5e08c668
kanaru-ssk <2019january.0131@gmail.com>
2022-07-11 19:23:13 +0900

    apollo client 導入

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,5 +1,11 @@
-const About = () => {
-  return <div>about</div>;
+import { PostsNode, PagesNode } from "types/topWordpressRes";
+
+type Props = {
+  aboutContent: string;
+};
+
+const About = ({ aboutContent }: Props) => {
+  return <div dangerouslySetInnerHTML={{ __html: aboutContent }}></div>;
 };
 
 export default About;
--- src/components/Works/index.tsx
+++ src/components/Works/index.tsx
@@ -1,5 +1,21 @@
-const Works = () => {
-  return <div>works</div>;
+import { PostsNode } from "types/topWordpressRes";
+
+type Props = {
+  worksPosts: PostsNode[];
+};
+
+const Works = ({ worksPosts }: Props) => {
+  return (
+    <div>
+      {worksPosts.map((value) => {
+        return (
+          <div className="h-24" key={value.id}>
+            {value.title}
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Works;
