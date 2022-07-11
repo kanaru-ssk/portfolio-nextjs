636179f596098f400d521289a67988383fabf3dc
kanaru-ssk <2019january.0131@gmail.com>
2022-07-11 09:47:43 +0900

    サイトマップをmapでループ

--- src/components/common/Footer/SiteMap.tsx
+++ src/components/common/Footer/SiteMap.tsx
@@ -1,21 +1,23 @@
 import MenuLink from "./MenuLink";
 
 const SiteMap = () => {
+  const pages = [
+    { path: "about" },
+    { path: "works" },
+    { path: "blog" },
+    { path: "contact" },
+  ];
+
   return (
-    <div className="flex flex-wrap justify-center gap-8 px-4 py-8">
-      <div className="w-36 text-center">
-        <MenuLink path="about" />
-      </div>
-      <div className="w-36 text-center">
-        <MenuLink path="works" />
-      </div>
-      <div className="w-36 text-center">
-        <MenuLink path="blog" />
-      </div>
-      <div className="w-36 text-center">
-        <MenuLink path="contact" />
-      </div>
-    </div>
+    <ul className="flex flex-wrap justify-center gap-8 px-4 py-8">
+      {pages.map((value) => {
+        return (
+          <li key={value.path} className="w-36 text-center">
+            <MenuLink path={value.path} />
+          </li>
+        );
+      })}
+    </ul>
   );
 };
 
