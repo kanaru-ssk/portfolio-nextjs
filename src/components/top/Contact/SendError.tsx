391633d4f69a10f5ded066d410f36c8affea432a
kanaru-ssk <2019january.0131@gmail.com>
2022-07-13 19:06:20 +0900

    コンタクトフォームのメッセージを調整

--- src/components/top/Contact/SendError.tsx
+++ src/components/top/Contact/SendError.tsx
@@ -1,9 +1,14 @@
-type Props = {
-  text: string;
-};
-
-const SendError = ({ text }: Props) => {
-  return <p className="whitespace-pre-wrap text-red">{text} </p>;
+const SendError = () => {
+  return (
+    <article>
+      <p className="font-bold text-red">送信失敗</p>
+      <p>
+        現在障害が発生している可能性がございます。
+        恐れ入りますが、ご連絡は下記メールアドレスに送信して下さい。
+      </p>
+      <p>contact@kanaru.jp</p>
+    </article>
+  );
 };
 
 export default SendError;
