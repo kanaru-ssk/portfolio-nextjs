26118854e928594f20452ebeaaef28619f3ffd66
kanaru-ssk <2019january.0131@gmail.com>
2022-07-15 02:35:18 +0900

    404.webpを追加

--- src/components/top/Tab/Blog.tsx
+++ src/components/top/Tab/Blog.tsx
@@ -20,7 +20,7 @@ const Blog = ({ blogPosts }: Props) => {
                     src={
                       value.featuredImage?.node.sourceUrl
                         ? value.featuredImage.node.sourceUrl
-                        : "/img/ogp.webp"
+                        : "/img/404.webp"
                     }
                     width="128"
                     height="72"
